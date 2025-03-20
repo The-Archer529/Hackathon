e49f77bd6a7048eefb0ddfffb4897b6bb1335e61
Tijmen Veldhuizen <206292@student.aventus.nl>
2025-03-20 11:12:50 +0100

    base code

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function showOptions(id) {
+    if(id == "option1b") {
+        document.getElementById("option1").classList.add("hidden")
+    }
+    else {
+        document.getElementById(id).classList.remove("hidden");
+    }
+}
