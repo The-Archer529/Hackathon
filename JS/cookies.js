3e615f148325095087ad56afe472badf3b6e1740
thymon26 <djthymen1@gmail.com>
2025-03-21 14:13:18 +0100

    punten

---
+++ JS/cookies.js
@@ -0,0 +1,32 @@
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function updatePoints(points) {
+    let currentPoints = parseInt(getCookie("points")) || 0;
+    currentPoints += points;
+    setCookie("points", currentPoints, 7);
+}
+
+function resetPoints() {
+    setCookie("points", 0, 7);
+}
--- JS/level1.js
+++ JS/level1.js
@@ -13,13 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     emails.forEach(email => {
         email.addEventListener('click', () => {
-
             if (email.dataset.phishing === 'true') {
                 result.textContent = 'Goed gedaan! Je hebt de phishing mail gevonden.';
                 result.style.color = 'green';
+                updatePoints(1); // Voeg een punt toe
+                setTimeout(() => {
+                    window.location.href = "level2.html"; // Ga naar het volgende level
+                }, 2000);
             } else {
                 result.textContent = 'Dit is geen phishing mail. Probeer het opnieuw.';
                 result.style.color = 'red';
+                updatePoints(-1); // Trek een punt af
             }
         });
     });
