56b99b8f31119e70a2136507ea1fb0131bc87187
thymon26 <djthymen1@gmail.com>
2025-03-21 14:19:04 +0100

    Update level3.js

--- JS/level3.js
+++ JS/level3.js
@@ -4,7 +4,12 @@ function connectWiFi(isSecure) {
         setTimeout(() => {
             document.getElementById('data-leak').classList.remove('hidden');
         }, 2000);
+        updatePoints(-1); // Trek een punt af
     } else {
         alert('✅ Je bent veilig verbonden! Dit netwerk is versleuteld en beschermt je gegevens.');
+        updatePoints(1); // Voeg een punt toe
+        setTimeout(() => {
+            window.location.href = "level4.html"; // Ga naar het volgende level
+        }, 2000);
     }
 }
